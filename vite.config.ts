21155a31ec3329b112b687301c85b9c24be2fbeb
apsdeepakkumar <71958929+apsdeepakkumar@users.noreply.github.com>
2026-01-27 00:34:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-// vite.config.ts
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deepakworks/",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  base: './',  // crucial for Netlify deployment; makes all asset paths relative
+  build: {
+    outDir: 'dist',  // your production folder
   },
-});
+})
