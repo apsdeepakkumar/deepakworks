d347a8c6d338b5e7108dcec6473799d85fa7aac6
apsdeepakkumar <71958929+apsdeepakkumar@users.noreply.github.com>
2026-01-26 23:26:22 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,14 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/deepakworks/", // 👈 REQUIRED for GitHub Pages
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
-
+export default defineConfig({
+  base: "/deepakworks/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
